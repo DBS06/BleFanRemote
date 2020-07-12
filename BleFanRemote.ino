d0b606fe096224bc3b9ff0a6b6fa9267cea29e98
phst <philipp.steiner@zuehlke.com>
2020-07-12 13:14:38 +0200

    POC

--- BleFanRemote.ino
+++ BleFanRemote.ino
@@ -1,6 +1,25 @@
 #include <ArduinoBLE.h>
-BLEService batteryService("1101");
-BLEUnsignedCharCharacteristic batteryLevelChar("2101", BLERead | BLENotify);
+
+BLEService fanCtrlService("1101");
+BLEBooleanCharacteristic fanPower("2101", BLERead | BLEWrite | BLENotify);
+
+struct FanCtrl
+{
+    bool power;
+    uint8_t speed;
+    bool turn;
+    uint8_t state;
+};
+
+FanCtrl fanCtrl = {false, 0, false, 0};
+
+size_t keepAlivePrevMs = 0;
+size_t keepAliveIntervalMs = 500;
+
+size_t blePrevMs = 0;
+size_t bleIntervalMs = 200;
+
+bool isIntervalOver(size_t prevMs, const size_t interval);
 
 void setup()
 {
@@ -9,6 +28,7 @@ void setup()
         ;
 
     pinMode(LED_BUILTIN, OUTPUT);
+
     if (!BLE.begin())
     {
         Serial.println("starting BLE failed!");
@@ -16,10 +36,10 @@ void setup()
             ;
     }
 
-    BLE.setLocalName("BatteryMonitor");
-    BLE.setAdvertisedService(batteryService);
-    batteryService.addCharacteristic(batteryLevelChar);
-    BLE.addService(batteryService);
+    BLE.setLocalName("FanControl");
+    BLE.setAdvertisedService(fanCtrlService);
+    fanCtrlService.addCharacteristic(fanPower);
+    BLE.addService(fanCtrlService);
 
     BLE.advertise();
     Serial.println("Bluetooth device active, waiting for connections...");
@@ -31,23 +51,48 @@ void loop()
 
     if (central)
     {
-        uint8_t battery = 0;
         Serial.print("Connected to central: ");
         Serial.println(central.address());
         digitalWrite(LED_BUILTIN, HIGH);
 
         while (central.connected())
         {
+            if (isIntervalOver(blePrevMs, bleIntervalMs))
+            {
+                fanCtrl.power = fanPower.value();
+                // fanPower.writeValue(batteryLevel);
+            }
 
-            battery++;
-            int batteryLevel = map(battery, 0, 255, 0, 100);
-            Serial.print("Battery Level % is now: ");
-            Serial.println(batteryLevel);
-            batteryLevelChar.writeValue(batteryLevel);
-            delay(200);
+            if (fanCtrl.power)
+            {
+                Serial.println("Power On");
+                digitalWrite(LED_BUILTIN, HIGH);
+            }
+            else
+            {
+                Serial.println("Power Off");
+                digitalWrite(LED_BUILTIN, LOW);
+            }
+        }
+        // digitalWrite(LED_BUILTIN, LOW);
+        if (isIntervalOver(keepAlivePrevMs, keepAliveIntervalMs))
+        {
+            Serial.print("Disconnected from central: ");
+            Serial.println(central.address());
         }
     }
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.print("Disconnected from central: ");
-    Serial.println(central.address());
+}
+
+bool isIntervalOver(size_t prevMs, const size_t interval)
+{
+    bool ret = false;
+    size_t curMs = millis();
+
+    if (curMs - prevMs >= interval)
+    {
+        prevMs = curMs;
+        ret = true;
+    }
+
+    return ret;
 }
